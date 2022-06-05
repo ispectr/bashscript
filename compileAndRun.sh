ea5130aab20b967e76a59c5adc5948430a5ca0e5
ispectr <51738701+ispectr@users.noreply.github.com>
2022-06-05 22:43:38 +0400

    Update compileAndRun.sh

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,70 +1,69 @@
 #!/bin/bash
 
-file_dir_flag=false
-output_file_dir_flag=false
-get_child_arg_flag=false
-output_file_name="a.out"
+input_file="main.c"
+output_file="a.out"
+outfile_basename="a.out"
+arguments=""
+wait_user=false
+positional_args=()
 wait_user=false
 
-file_dir=""
-output_file_dir=$PWD/a.out
-child_arg=""
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--input-file)
+      input_file="$2"
+      if [[ $2 != /* ]]; then
+        input_file=$PWD/$2
+      fi
+      infile_basename=$(basename $2)
+      shift
+      shift
+      ;;
 
-# PARSE ARGUMENTS
-for arg in "$@"; do
-  
-  # GET INPUT FILE:
-  if [[ $file_dir_flag == true ]]; then
-    file_dir=$arg
-    if [[ $arg != /* ]]; then
-      file_dir=$PWD/$arg
-    fi
-    file_dir_flag=false
-  fi
+    -o|--output-file)
+      output_file="$2"
+      if [[ $2 != /* ]]; then
+        output_file=$PWD/$2
+      fi
+      outfile_basename=$(basename $2)
+      shift
+      shift
+      ;;
 
-  if [[ $arg == "-i" ]] || [[ $arg == "--input-file" ]]; then
-    file_dir_flag=true
-  fi
+    -a|--arguments)
+      arguments="$2"
+      shift
+      shift
+      ;;
 
-  # GET OUTPUT FILE:
-  if [[ $output_file_dir_flag == true ]]; then
-    output_file_dir=$arg
-    if [[ $arg != /* ]]; then
-      output_file_dir=$PWD/$arg
-      output_file_name=$arg
-    else
-      output_file_name=$(basename $arg)
-    fi
-    output_file_dir_flag=false
-  fi
+    -w|--wait-user)
+      wait_user=true
+      shift
+      ;;
 
-  if [[ $arg == "-o" ]] || [[ $arg == "--output-file" ]]; then
-    output_file_dir_flag=true
-  fi
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
 
-  # GET CHILD ARGUMENTS
-  if [[ $get_child_arg_flag == true ]]; then
-    child_arg=$arg
-    get_child_arg_flag=false
-  fi
-
-  if [[ $arg == "-a" ]] || [[ $arg == --arguments ]]; then
-    get_child_arg_flag=true
-  fi
-
-  if [[ $arg == "-w" ]] || [[ $arg == "--wait-user" ]]; then
-    wait_user=true
-  fi
+    *)
+      positional_args+=($1)
+      shift
+      ;;
+  esac
 done
 
-if [[ -f "$file_dir" ]]; then
-  g++ $file_dir -o $output_file_dir
-  xreturnval=$?
-  if [[ $xreturnval == 0 ]]; then 
-    $PWD/./$output_file_name $child_arg
+echo "input file = $input_file"
+echo "ouput file = $output_file"
+echo "arguments  = $arguments"
+
+if [[ -f "$input_file" ]]; then
+  g++ $input_file -o $output_file
+  if [[ $? == 0 ]]; then
+    $PWD/./$outfile_basename $arguments
   fi
-else 
-  echo "[-] no such file or directory: $file_dir"
+else
+  echo "[-] no such file or directry: $input_file"
 fi
 
 if [[ $wait_user == true ]]; then
